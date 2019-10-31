8e8b1d740383e63202aa2e72d2df8d4f1b08e290
Ricardo <xinrun0229@gmail.com>
2019-10-31 00:10:00 -0500

    Update game_server.py

--- game_server.py
+++ game_server.py
@@ -2,7 +2,24 @@
 This is the python file for creating a game server.
 '''
 
-#import socket
+import socket
 
+# Open the file...
+# Read the file by lines...
 f = open('quiz.txt', 'r')
-lines = f.readlines().split(',')
+
+# Variable 'lines' is a tuple with each element is a string and each string contains a
+# line in the txt file.
+lines = f.readlines()
+
+# Split the 'lines' into two tuples 'Question' and 'Answer'...
+Question = []
+Answer = []
+for line in lines:
+    piece = line.split(',')
+    Question.append(piece[0])
+    Answer.append(piece[1])
+
+print(lines)
+print(Question)
+print(Answer)
