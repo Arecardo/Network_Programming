c72f4474879200b75861374e219383d640afed8d
Ricardo <xinrun0229@gmail.com>
2019-10-31 02:27:38 -0500

    Update game_server.py. Finish the data loading part.

--- game_server.py
+++ game_server.py
@@ -6,7 +6,7 @@ Description: This is the python file for the game server.
 
 import socket
 
-def load_data(Question, Answer):
+def load_data(questions, answers):
     # Open the file
     f = open('quiz.txt', 'r')
 
@@ -20,5 +20,12 @@ def load_data(Question, Answer):
     # e.g. Answer[0] is the answer for Question[0]
     for line in lines:
         piece = line.split(',')
-        Question.append(piece[0])
-        Answer.append(piece[1][:-2])
+        questions.append(piece[0])
+        answers.append(piece[1][:-2])
+
+if __name__ == "__main__":
+    questions = []
+    answers = []
+    load_data(questions, answers)
+    print(questions)
+    print(answers)
