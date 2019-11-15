4d1763d301ac09919d23bf53fc7be6432f0f88ed
Ricardo <xinrun0229@gmail.com>
2019-11-15 15:00:25 -0600

    update main.cpp

--- Algorithm_Project/main.cpp
+++ Algorithm_Project/main.cpp
@@ -12,7 +12,7 @@ void load_data(){
 
 }
 
-generate_demo(vector<int> &temp_test_v, vector<int> &temp_test_w){
+void generate_demo(vector<int> &temp_test_val, vector<int> &temp_test_wt){
   // use array to store data
   int a = [3, 4, 15, 4, 21, 6, 8, 9, 1, 12];
   int b = [2, 3, 7, 3, 15, 4, 5, 3, 1, 6];
@@ -20,21 +20,39 @@ generate_demo(vector<int> &temp_test_v, vector<int> &temp_test_w){
   //assign data to the vectors
   for(int i=0; i<10; i++)
   {
-    temp_test_v.push_back(a[i]);
-    temp_test_w.push_back(b[i]);
+    temp_test_val.push_back(a[i]);
+    temp_test_wt.push_back(b[i]);
   }
-
 }
 
-void get_total_value(int v, int w){
+class solution{
+public:
+  /*
+  v: the length of the vector v
+  w: the length of the vector w
+  return: solution_table - a 2-d vector object
+  */
+  vector<vector<int>> get_solution(vector<int> &val, vector<int> &wt){
+    //initial  solution_table
+    vector<vector<int>> solution_table;
+    len_val = val.size();
+    len_wt = wt.size();
 
+    // return the result
+    return solution_table;
+  }
 }
 
 int main(){
   // generate data for demo case
-  vector<int> temp_test_v;
-  vector<int> temp_test_w;
-  generate_demo(temp_test_v, temp_test_w);
+  vector<int> temp_test_val;
+  vector<int> temp_test_wt;
+  generate_demo(temp_test_val, temp_test_wt);
 
+  // create solution object
+  // get solution_table
+  vector<vector<int>> demo_solution_table;
+  solution demo_solution;
+  demo_solution_table = demo_solution.get_solution(temp_test_val, temp_test_wt);
   return 0;
 }
