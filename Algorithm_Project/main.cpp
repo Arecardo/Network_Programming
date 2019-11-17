2149c6fb9e8107b2a16a0b1712f104a291c66e55
xinrun <zhan3031@pnw.edu>
2019-11-16 18:28:36 -0600

    update main.cpp

--- Algorithm_Project/main.cpp
+++ Algorithm_Project/main.cpp
@@ -32,27 +32,66 @@ public:
   w: the length of the vector w
   return: solution_table - a 2-d vector object
   */
-  vector<vector<int>> get_solution(vector<int> &val, vector<int> &wt){
-    //initial  solution_table
-    vector<vector<int>> solution_table;
+  vector<vector<int>> get_solution(vector<int> &val, vector<int> &wt, int capacity_limit){
+    // get the size of val and wt
     len_val = val.size();
     len_wt = wt.size();
+
+    // initial the solution table,
+    // the size of table is (len_val)*(capacity+1)
+    vector<vector<int>> solution_table(len_val+1, vector<int>(capacity_limit+1));
+
+    // assign 0 to the first row and the first column of the table
+    for(int i = 0; i < len_val+1; i++){
+      solution_table[i][0] = 0;
+    }
+
+    for(int i = 0; i < capacity+1; i++){
+      solution_table[0][i] = 0;
+    }
+
+    // iteratively generate the value of each cell
+    for(int item = 1; item < len_val+1; i++){
+      for(int curr_cap = 1; curr_cap < capacity+1; j++){
+        int max_val_without_curr = solution_table[item - 1][curr_cap]; // this is always exist
+        int max_with_curr = 0; // initial the value to 0
+
+        int curr_wt = wt[item-1]; // get the current item's weight
+        if(curr_cap >= curr_wt){ // check if the knapsack can include the current item
+          max_val_with_curr = val[item - 1]; // if it is, get the current value of the item
+
+          // get the remaining capacity if we include the current item in the knapsack
+          int remain_cap = curr_cap - curr_wt;
+
+          // the maximum value with current item is:
+          // value of current item + maximum value obtainble with the remaining capacity
+          max_val_with_curr += solution_table[item - 1][remain_cap];
+        }
+        if (max_val_without_curr > max_val_with_curr){
+          solution_table[item][curr_cap] = max_val_without_curr;
+        }
+        else{
+          solution_table[item][curr_cap] = max_val_with_curr;
+        }
+      }
+    }
 
     // return the result
     return solution_table;
   }
-}
+};
 
 int main(){
   // generate data for demo case
   vector<int> temp_test_val;
   vector<int> temp_test_wt;
   generate_demo(temp_test_val, temp_test_wt);
+  int capacity_limit = 20;
 
   // create solution object
   // get solution_table
   vector<vector<int>> demo_solution_table;
   solution demo_solution;
-  demo_solution_table = demo_solution.get_solution(temp_test_val, temp_test_wt);
+  demo_solution_table = demo_solution.get_solution(temp_test_val, temp_test_wt, capacity_limit);
   return 0;
 }
