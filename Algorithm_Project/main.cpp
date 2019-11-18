974fd30925202a81be6c4c3d1b4a0841fb28f70f
Ricardo <xinrun0229@gmail.com>
2019-11-18 17:30:52 -0600

    updae main.cpp: finish demo part!

--- Algorithm_Project/main.cpp
+++ Algorithm_Project/main.cpp
@@ -8,38 +8,15 @@
 #include<vector>
 using namespace std;
 
-void load_data(){
-
-}
-
-void generate_demo(vector<int> &temp_test_val, vector<int> &temp_test_wt){
-  // use array to store data
-  int a[10] = {3, 4, 15, 4, 21, 6, 8, 9, 1, 12};
-  int b[10] = {2, 3, 7, 3, 15, 4, 5, 3, 1, 6};
-
-  //assign data to the vectors
-  for(int i=0; i<10; i++)
-  {
-    temp_test_val.push_back(a[i]);
-    temp_test_wt.push_back(b[i]);
-  }
-}
-
 class solution{
 public:
   /*
-  v: the length of the vector v
-  w: the length of the vector w
-  return: solution_table - a 2-d vector object
+  main solution class
   */
-  vector<vector<int>> get_solution(vector<int> &val, vector<int> &wt, int capacity_limit){
-    // get the size of val and wt
-    int len_val = val.size();
-    int len_wt = wt.size();
-
+  void get_solution(int val[], int wt[], int capacity_limit, int len_val){
     // initial the solution table,
     // the size of table is (len_val)*(capacity+1)
-    vector<vector<int>> solution_table(len_val+1, vector<int>(capacity_limit+1));
+    int solution_table[len_val+1][capacity_limit+1];
 
     // assign 0 to the first row and the first column of the table
     for(int i = 0; i < len_val+1; i++){
@@ -57,17 +34,21 @@ public:
         int max_val_with_curr = 0; // initial the value to 0
 
         int curr_wt = wt[item-1]; // get the current item's weight
+
         if(curr_cap >= curr_wt){ // check if the knapsack can include the current item
-          int max_val_with_curr = val[item - 1]; // if it is, get the current value of the item
+          max_val_with_curr = val[item - 1]; // if it is, get the current value of the item
 
           // get the remaining capacity if we include the current item in the knapsack
           int remain_cap = curr_cap - curr_wt;
 
           // the maximum value with current item is:
           // value of current item + maximum value obtainble with the remaining capacity
-          max_val_with_curr += solution_table[item - 1][remain_cap];
+          max_val_with_curr = max_val_with_curr + solution_table[item - 1][remain_cap];
         }
-        if (max_val_without_curr > max_val_with_curr){
+
+        // compare the max_val_with_curr with max_val_without_curr
+        // assign the cell with the bigger one
+        if(max_val_without_curr > max_val_with_curr){
           solution_table[item][curr_cap] = max_val_without_curr;
         }
         else{
@@ -76,24 +57,75 @@ public:
       }
     }
 
-    // return the result
-    return solution_table;
+    // print out the result
+    cout << "The final result is: ";
+    cout << solution_table[len_val][capacity_limit] << endl;
+    cout << endl;
   }
 };
 
-int main(){
+void demo(){
+  // print out information
+  cout << "----------------------------------------------------" << endl;
+  cout << "Welcome to the demo mode!" << endl;
+  cout << "Initializing..." << endl;
+
   // generate data for demo case
-  vector<int> temp_test_val;
-  vector<int> temp_test_wt;
-  generate_demo(temp_test_val, temp_test_wt);
+  int temp_test_val[10] = {3, 4, 15, 4, 21, 6, 8, 9, 1, 12};
+  int temp_test_wt[10] = {2, 1, 7, 3, 15, 4, 5, 3, 1, 6};
   int capacity_limit = 20;
+  int len_val = sizeof(temp_test_val)/sizeof(temp_test_val[0]);
+
+  // print out information
+  cout << "In this demo, there are 10 available items." << endl;
+  cout << "weight for each item: ";
+  for(int i=0; i<10; i++){
+    cout << temp_test_wt[i] << " ";
+  }
+  cout << endl;
+  cout << "value for each item: ";
+  for(int i=0; i<10; i++){
+    cout << temp_test_val[i] << " ";
+  }
+  cout << endl;
+  cout << "The capacity of the bag is: " << capacity_limit << endl;
 
   // create solution object
   // get solution_table
-  vector<vector<int>> demo_solution_table;
+  cout << endl << "Calculating..." << endl;
+  int demo_solution_table[11][11];
   solution demo_solution;
-  demo_solution_table = demo_solution.get_solution(temp_test_val, temp_test_wt, capacity_limit);
-  cout << demo_solution_table[10][10] << endl;
+  demo_solution.get_solution(temp_test_val, temp_test_wt, capacity_limit, len_val);
+  cout << "The domo is done! Thank you! " << endl;
+  cout << "----------------------------------------------------" << endl;
+}
 
+int main(){
+  cout << "****************************************************" << endl;
+  cout << "*           Welcome to the knapsack program        *" << endl;
+  cout << "*                Author: Xinrun Zhang              *" << endl;
+  cout << "****************************************************" << endl;
+  while(true){
+    cout << "Please select a mode to demonstrate a demo or input your own data: " << endl;
+    cout << "1. demo" << endl;
+    cout << "2. self-defined 0-1 knapsack problem" << endl;
+    cout << "3. quit" << endl;
+    cout << "Your choice: ";
+
+    int user_choice;
+    cin >> user_choice;
+    if(user_choice == 1){
+      demo();
+    }
+    else if(user_choice == 2) {
+      cout << "Sorry, I'm still working on this part!" << endl;
+    }
+    else if(user_choice == 3){
+      break;
+    }
+    else{
+      cout << "Incorrect Input! Please choose the mode! " << endl;
+    }
+  }
   return 0;
 }
