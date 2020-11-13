713db44a2fd2979211dffbdd85804c290627def1
Ricardo <xinrun0229@gmail.com>
2020-11-13 17:37:35 -0600

    update

---
+++ UNP/mult_process_sc/blocking_server.cpp
@@ -0,0 +1,94 @@
+/*
+ * multi-process tcp server
+ * date: 11/06/2020
+*/
+
+#include<iostream>
+#include<sys/socket.h> // socket
+#include<netinet/in.h> //sockaddr
+#include<arpa/inet.h> // inet_pton
+#include<string.h> // memset
+#include<unistd.h> // fork, exec, close
+#include<netdb.h> // getnameinfo
+#include<string>
+
+int main(int argc, char** argv){
+    std::cout<<"Start the server..."<<std::endl;
+    
+    // create listening socket
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listenfd == -1){ // if fail to create listening socket
+        std::cout<<"Can't create listening socket!"<<std::endl;
+        return -1;
+    }
+
+    // set listening address
+    struct sockaddr_in lisenaddr;
+    memset(&lisenaddr, 0, sizeof(lisenaddr));                   // initial to all 0
+    lisenaddr.sin_family = AF_INET;                             // family
+    lisenaddr.sin_port = htons(9877);                           // port
+    inet_pton(AF_INET, "127.0.0.1", &lisenaddr.sin_addr);       // ip address
+    
+    // bind listening address to listening
+    if(bind(listenfd, (sockaddr *)& lisenaddr, sizeof(lisenaddr)) == -1){
+        std::cout<<"Can't bind listening sockaddr to listenfd!"<<std::endl;
+        return -2;
+    }
+
+    // indicate the server starts listen
+    std::cout<<"Start listening..."<<std::endl;
+    
+    // active listenfd to listen
+    if(listen(listenfd, SOMAXCONN) == -1){
+        std::cout<<"Can't make listenfd to listen!"<<std::endl;
+        return -3;
+    }
+
+    // accept connection
+    struct sockaddr_in connaddr;
+    socklen_t connlen;
+    int connfd = accept(listenfd, (sockaddr*)& connaddr, &connlen);
+    if(connfd == -1){
+        std::cout<<"Problem with client connection!"<<std::endl;
+        return -4;
+    }
+
+    // close listening
+    close(listenfd);
+
+    // get ip address and port number of the connection
+    char host[NI_MAXHOST];
+    char svrc[NI_MAXSERV];
+    inet_ntop(AF_INET, &connaddr.sin_addr, host, NI_MAXHOST);
+    std::cout<<host<<" connected on"<<ntohs(connaddr.sin_port)<<std::endl;
+
+    // read message from connfd
+    char buf[4096];
+    while(true){
+        // clear buf
+        memset(&buf, 0, 4096);
+
+        // wait for a message
+        int byteRecv = recv(connfd, &buf, 4096, 0);
+        if(byteRecv == -1){ // if recv failed
+            std::cout<<"There is a connection issue!"<<std::endl;
+            break;
+        } 
+
+        if(byteRecv == 0){ // if recv nothing
+            std::cout<<"Client disconnected!"<<std::endl;
+            break;
+        }
+
+        // display the message
+        std::cout<<"Recieved: "<<buf<<std::endl;
+
+        // send the message back
+        send(connfd, buf, byteRecv+1, 0);
+    }
+
+    // close connfd
+    close(connfd);
+    
+    return 0;
+}
---
+++ UNP/mult_process_sc/locking_client.cpp
@@ -0,0 +1,66 @@
+/*
+ * multi-process tcp client
+ * date: 11/08/2020
+*/
+
+#include<iostream>
+#include<sys/socket.h> // socket
+#include<netinet/in.h> //sockaddr
+#include<arpa/inet.h> // inet_pton
+#include<unistd.h> // fork, exec, close
+#include<string.h> // memset
+#include<netdb.h> // getnameinfo
+#include<string>
+
+
+int main(int argc, char** argv){
+    
+    // create client socket
+    int clifd = socket(AF_INET, SOCK_STREAM, 0);
+    if(clifd == -1){ // if fail to create client socket
+        std::cout<<"Can't create client socket"<<std::endl;
+        return -1;
+    }
+
+    // set server address
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(9877);
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+
+    // connect to the server
+    int ifcon = connect(clifd, (sockaddr*)&servaddr, sizeof(servaddr));
+    if(ifcon == -1){
+        std::cout<<"Connection failed!"<<std::endl;
+        return -2;
+    }
+
+    // send and recv mesg loop
+    char buf[4096];
+    std::string userInput;
+    while(true){
+        // enter the mesg
+        std::cout<<"> ";
+        std::getline(std::cin, userInput);
+
+        // send the mesg
+        int ifsend = send(clifd, userInput.c_str(), userInput.size()+1, 0);
+        if(ifsend == -1){
+            std::cout<<"Can't send the message to the server!"<<std::endl;
+            continue;
+        }
+
+        // get the mesg back
+        memset(&buf, 0, sizeof(buf));
+        int byteReceived = recv(clifd, buf, 4096, 0);
+
+        // output the mesg
+        std::cout<<"SERVER> "<<std::string(buf, byteReceived)<<std::endl;
+    }
+
+    // close the client socket
+    close(clifd);
+
+    return 0;
+}
